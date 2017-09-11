fcd1b808fca8e0c6a5b31fd1e97cf6e26e526930
Marcin Matuszkiewicz <marcin201@gmail.com>
2017-09-11 15:56:05 -0700

    Added processing for eCheck deposits

--- README.txt
+++ README.txt
@@ -1,3 +1,7 @@
-When viewing transactions click on "Check Number".  This will create a filename in the formation
+when viewing transactions of a cashed check click on "Check Number".  This will create a filename in the formation
 'check #### posted yyyy-mm-dd'
 and put it to clipboard.  The print check and use the finename in the clipboard to save it as PDF.
+
+When viewing a transaction of a eCheck deposit click on "Ammount".  This will creat a file name in the format
+'posted yyyy-mm-dd amount $$$$_cc'
+and put copy it to clipboard. Print the transaction and use the filenem in the clipboard to save it as PDF.
--- content.js
+++ content.js
@@ -8,7 +8,21 @@ $(document).on('click', function (e) {
     var dateTxt = $p.find(':contains("Posting Date:")').find('span').text();
     var date = new Date(dateTxt);
     
-    var fname = 'check '+checkNum+' posted '+date.format('yyyy-mm-dd');
+    //var fname = 'check '+checkNum+' posted '+date.format('yyyy-mm-dd');
+    var fname = 'posted '+date.format('yyyy-mm-dd')+' check '+checkNum;
+    console.log(fname);
+    GM_setClipboard(fname);
+  } else if (text == 'Amount:') {
+    var amount = $t.parent().find('span').text().trim();
+    var $p = $t.parent().parent();
+    var dateTxt = $p.find(':contains("Posting Date:")').find('span').text();
+    var date = new Date(dateTxt);
+    
+    amount = amount.replace('$', '');
+    amount = amount.replace(',', '');
+    amount = amount.replace('.', '_');
+    
+    var fname = 'posted '+date.format('yyyy-mm-dd')+' amount '+amount;
     console.log(fname);
     GM_setClipboard(fname);
   }
